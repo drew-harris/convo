0d5643d6fb9f4f900b87ad580a05ccd7a7b116af
drew-harris <dsharris10@gmail.com>
2021-08-03 15:27:29 -0500

    Add working posting button
    TODO: Resolve post schema before merge

--- src/components/HomeRoutes/Feed/NewPost/NewPost.js
+++ src/components/HomeRoutes/Feed/NewPost/NewPost.js
@@ -7,10 +7,46 @@ const NewPost = (props) => {
 
   const [pickerData, setPickerData] = useState(null);
   const [selected, setSelected] = useState(null);
+  const [postText, setPostText] = useState("");
 
   const pickerChange = (selection) => {
     console.log(selection);
     setSelected(selection);
+  };
+
+  const addPost = async () => {
+    try {
+      if (selected === null) {
+        throw new Error("Please select a group to post to.");
+      }
+      if (postText.length < 3) {
+        throw new Error("Please enter text to post");
+      }
+
+      let groupRef = db.collection("groups").doc(selected.id);
+      const snap = await groupRef.get();
+      const members = await snap.get("members");
+      console.log(members);
+
+      let docref = db
+        .collection("groups")
+        .doc(selected.id)
+        .collection("posts")
+        .doc();
+
+      //TODO: ADD TIME CREATED
+      docref.set({
+        id: docref.id,
+        members: members,
+        groupRef: groupRef,
+        groupName: selected.name,
+        groupId: selected.id,
+        creator: auth.currentUser.displayName,
+      });
+    } catch (err) {
+      console.error(err.message);
+      alert(err.message);
+    }
   };
 
   useEffect(() => {
@@ -51,9 +87,13 @@ const NewPost = (props) => {
           cols="auto"
           rows={3}
           className="newpost-input"
+          value={postText}
+          onChange={(event) => setPostText(event.target.value)}
         />
 
-        <div className="newpost-createbutton">Post</div>
+        <div className="newpost-createbutton" onClick={addPost}>
+          Post
+        </div>
       </div>
     );
   } else {
